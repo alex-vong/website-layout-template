d2f21fdd1d3e98eb7500023ad16ebe5e688fc61f
Alex Vong <avong003@gmail.com>
2022-08-31 20:39:13 -0700

    created index and css files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html class="no-js">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Responsive Website Design Template</title>
+    <meta name="description" content="[[Meta Description]]" />
+    <meta name="og:description" content="[[Meta Description]]]" />
+    <meta property="og:title" content="Alex[] Codes a Thing" />
+    <meta property="og:type" content="">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="[[Meta Image]]]" />
+    <link rel="shortcut icon" type="image/jpg" href="[[favicon]]" />
+    <link rel="stylesheet" href="css/styles.css" />
+</head>
+
+<body>
+</body>
+
+</html>
