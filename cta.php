b6df592caba1230e3b91abe307a23177fb4d3aa6
Alex Vong <avong003@gmail.com>
2022-09-03 19:25:09 -0700

    created cta

---
+++ cta.php
@@ -0,0 +1,16 @@
+<inner-column>
+	<section class="cta">
+		<h2 class="desc-fs">
+			This module is a "call to action."
+		</h2>
+
+		<p class="text-fs">
+			This is a pretty standard part of any website. It usually has some cool heading (like above) along with a description (that's me!) as well as a button (below)
+		</p>
+
+		<button>
+			<a href="" class="button text-fs">Here's the action!</a>
+		</button>
+		
+	</section>
+</inner-column>
