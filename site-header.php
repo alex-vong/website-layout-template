13fb65b945f850a7903747a99e763aa9075eccf6
Alex Vong <avong003@gmail.com>
2022-09-03 19:24:15 -0700

    added site header php

---
+++ site-header.php
@@ -0,0 +1,14 @@
+
+<inner-column>
+    <masthead>
+            <nav class="site-logo">
+                <a href="/" class="logo title-fs">Logo</a>
+            </nav>
+            <nav class="site-menu-bar">
+                <a href="" class="link-fs">Link 1</a>
+                <a href="" class="link-fs">Link 2</a>
+                <a href="" class="link-fs">Link 3</a>
+                <a href="" class="link-fs">Sign In</a>
+            </nav>
+    </masthead>
+</inner-column>
