668bd7bec3341340b1decf8493c37d752aaa8fa8
Alex Vong <avong003@gmail.com>
2022-09-03 19:25:52 -0700

    created footer

---
+++ footer.php
@@ -0,0 +1,36 @@
+<inner-column>
+
+	<section class="footer-container">
+		<h3 class="desc-fs">This is the site footer and it has a "site-map" module in it.</h3>
+
+		<p class="text-fs">
+			Pretty standard type of thing, right?
+		</p>
+
+		<section class="footer-grid">
+			<section class="fg-heading">
+				<a href="" class="link-fs">Logo</a>
+				<a href="" class="link-fs">Some Page</a>
+				<a href="" class="link-fs">Another Page</a>
+			</section>
+
+			<setion class="fg-user">
+				<a href="" class="link-fs">Sign-in</a>
+			</setion>
+
+			<section class="fg-lega;">
+				<a href="" class="link-fs">Legal Thing A</a>
+				<a href="" class="link-fs">Legal Thing B</a>
+			</section>
+
+			<section class="fg-social">
+				<a href="" class="link-fs">Social Thing A</a>
+				<a href="" class="link-fs">Social Thing B</a>
+				<a href="" class="link-fs">Social Thing C</a>
+				<a href="" class="link-fs">Social Thing D</a>
+
+			</section>
+		</section>	
+	</section>
+
+</inner-column>
