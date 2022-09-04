d6eeb52227ad87bb39197c19a660973df6cf6b76
Alex Vong <avong003@gmail.com>
2022-09-03 19:25:43 -0700

    created landing section

---
+++ landing-section.php
@@ -0,0 +1,15 @@
+<inner-column>
+    <section class="landing-container">
+        <picture>
+            <img src="https://peprojects.dev/images/square.jpg" alt="company-hero-image">
+        </picture>
+        <div class="landing-text">
+            <h1 class="main-heading-fs landing-header">
+                Hello! This is a 'diptych'!
+            </h1>
+            <p class="text-fs">
+                You can call it whatever you want. It's a module that has an image and some supporting text. On a smaller screen it works nicely to stack. On a larger screen it fits side by side.
+            </p>
+        </div>
+    </section>
+</inner-column>
