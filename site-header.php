d317fa2b28b42abe5dfce6af4b0e5ae3cd9208e4
Alex Vong <avong003@gmail.com>
2022-09-14 14:10:03 -0700

    changrd link font size to 16

--- site-header.php
+++ site-header.php
@@ -5,10 +5,10 @@
                 <a href="/" class="logo title-fs">Logo</a>
             </nav>
             <nav class="site-menu-bar">
-                <a href="" class="link-fs">Link 1</a>
-                <a href="" class="link-fs">Link 2</a>
-                <a href="" class="link-fs">Link 3</a>
-                <a href="" class="link-fs">Sign In</a>
+                <a href="" class="text-fs">Link 1</a>
+                <a href="" class="text-fs">Link 2</a>
+                <a href="" class="text-fs">Link 3</a>
+                <a href="" class="text-fs">Sign In</a>
             </nav>
     </masthead>
 </inner-column>
