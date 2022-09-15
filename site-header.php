285c5a41c0d7c0428b2cad0800448a3194f45cb1
Alex Vong <avong003@gmail.com>
2022-09-14 22:14:08 -0700

    added a ‘get started button’

--- site-header.php
+++ site-header.php
@@ -1,14 +1,21 @@
 
 <inner-column>
     <masthead>
+
+        <section class="top-stuff">
             <nav class="site-logo">
                 <a href="/" class="logo title-fs">Logo</a>
             </nav>
             <nav class="site-menu-bar">
-                <a href="" class="text-fs">Link 1</a>
-                <a href="" class="text-fs">Link 2</a>
-                <a href="" class="text-fs">Link 3</a>
-                <a href="" class="text-fs">Sign In</a>
+                <a href="" class="link-fs">Products</a>
+                <a href="" class="link-fs">Solutions</a>
+                <a href="" class="link-fs">Resources</a>
             </nav>
+        </section>
+
+        <section class="bottom-stuff">
+            <a href="" class="link-fs">Get Started</a>
+        </section>
+
     </masthead>
 </inner-column>
